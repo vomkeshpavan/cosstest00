64cdb42d6d320e053febcc9b3231a02af1e2e406
Ubuntu <ubuntu@ip-172-31-1-61.ap-south-1.compute.internal>
2025-03-12 16:57:46 +0000

    .sh added

---
+++ file.sh
@@ -0,0 +1,11 @@
+#!/bash/bin
+sudo apt update 
+sudo apt install openjdk-17-jdk -y
+sudo apt install maven -y
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+sudo apt-get install jenkins -y
